7083e141c8be3ba6e3a6c77b3f8fa1352cb2ae89
joss-cm <josselynegr31@gmail.com>
2025-03-09 00:38:47 -0500

    second question

---
+++ api-rest/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "⏳ Waiting for PostgreSQL to start..."
+sleep 5
+
+echo "🚀 Creating tables in the database..."
+python -c "from src.models import Department, Job, HiredEmployee; from src.database import Base, engine; Base.metadata.create_all(engine)"
+
+echo "🚀 Starting FastAPI Server..."
+exec uvicorn src.app:app --host 0.0.0.0 --port 8000 --reload
